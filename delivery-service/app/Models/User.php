04a7a1168aa1024bd5c4fef0cc676e9fc5825427
Thiago Melo <thiago_melo18@yahoo.com.br>
2025-08-06 19:54:53 -0300

    feat: installed laravel in delivery-service module.

---
+++ delivery-service/app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    protected $fillable = ['id', 'name', 'email', 'role'];
+
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+}
