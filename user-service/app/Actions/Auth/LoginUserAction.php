2cd6a87ae21f8e02afdd51650e91479c96042a24
Thiago Melo <thiago_melo18@yahoo.com.br>
2025-08-11 15:16:14 -0300

    refactor: altered method login action, add colunm role in return response autenticate to user-service.

--- user-service/app/Actions/Auth/LoginUserAction.php
+++ user-service/app/Actions/Auth/LoginUserAction.php
@@ -3,19 +3,40 @@
 namespace App\Actions\Auth;
 
 use App\DTOs\Auth\LoginUserDTO;
-use Illuminate\Support\Facades\Auth;
 use App\Exceptions\Auth\InvalidCredentialsException;
+use Illuminate\Support\Facades\Auth;
 
 class LoginUserAction
 {
     public function execute(LoginUserDTO $dto): string
     {
-        if (! $token = Auth::guard('api')->attempt([
+        /*
+            if (! $token = Auth::guard('api')->attempt([
+                'email' => $dto->email,
+                'password' => $dto->password
+            ])) {
+                throw new InvalidCredentialsException();
+            }
+        */
+
+        $credentials = [
             'email' => $dto->email,
-            'password' => $dto->password
-        ])) {
+            'password' => $dto->password,
+        ];
+
+        if (!$user = Auth::guard('api')->getProvider()->retrieveByCredentials($credentials)) {
             throw new InvalidCredentialsException();
         }
+
+        if (!Auth::guard('api')->validate($credentials)) {
+            throw new InvalidCredentialsException();
+        }
+
+        $role = $user->getRoleNames()->first() ?? null;
+
+        $token = Auth::guard('api')->claims([
+            'role' => $role,
+        ])->login($user);
 
         return $token;
     }
