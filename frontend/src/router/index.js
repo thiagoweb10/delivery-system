16b2801ab9d629bc82ef4b70ee0ab5f70028dc59
Thiago Melo <thiago_melo18@yahoo.com.br>
2025-09-20 05:54:47 -0300

    refactor(reset-password): improve password reset flow and strength indicator front-end.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,19 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "@/views/Dashboard.vue";
 import Preview from "@/views/Preview.vue";
 import Login from "@/views/Login.vue";
+import ResetPasswordLink from "@/views/ResetPasswordLink.vue";
+import ResetPassword from "@/views/ResetPassword.vue";
+import ResetNewPassword from "@/views/ResetNewPassword.vue";
+
+
 
 const routes = [
-  { path: "/login",       name: "login",              component: Login },
-  { path: "/",            name: "delivery-dashboard", component: Dashboard },
-  { path: "/dashboard",   name: "delivery-dashboard", component: Dashboard },
-  { path: "/detalhes/:id?",name: "delivery-details", component: Preview, props: true },
+
+  { path: "/login",         name: "login",              component: Login },
+  { path: "/",              name: "delivery-dashboard", component: Dashboard },
+  { path: "/dashboard",     name: "delivery-dashboard", component: Dashboard },
+  { path: "/detalhes/:id?", name: "delivery-details",   component: Preview, props: true },
+  { path: "/nova-senha",   name: "reset-password-new", component: ResetNewPassword },
 
 ];
 
